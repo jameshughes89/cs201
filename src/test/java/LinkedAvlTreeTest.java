3583d15c9efefa8a5305976a03e291451de3cf99
James Hughes <jameshughes89@gmail.com>
2022-11-10 15:30:23 -0400

    :shrug:

--- src/test/java/LinkedAvlTreeTest.java
+++ src/test/java/LinkedAvlTreeTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.*;
+import org.junit.runner.notification.RunListener;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -21,7 +22,6 @@ public class LinkedAvlTreeTest {
             assertThat(classUnderTest.size())
                     .isEqualTo(0);
         }
-
 
         @Nested
         class WhenSingleton {
