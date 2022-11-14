c368259b34f2fffdb0d698e49e49929ee790c3b0
James Hughes <jameshughes89@gmail.com>
2022-11-14 12:40:07 -0400

    remove public from interfaces

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -6,7 +6,7 @@ public interface Heap<T extends Comparable<? super T>> extends Iterable<T> {
      * @param element Element to be added to the heap.
      * @return True if the element was added successfully, False otherwise.
      */
-    public boolean add(T element);
+    boolean add(T element);
 
     /**
      * Remove the element at the top of the heap. This may be the min or max element depending on the specific heap
@@ -14,7 +14,7 @@ public interface Heap<T extends Comparable<? super T>> extends Iterable<T> {
      *
      * @return Element at the top of the heap.
      */
-    public T remove();
+    T remove();
 
     /**
      * Return the element at the top of the heap, but leave it at the top of the heap. This element may be the min or
@@ -22,12 +22,12 @@ public interface Heap<T extends Comparable<? super T>> extends Iterable<T> {
      *
      * @return Element at the top of the heap.
      */
-    public T peek();
+    T peek();
 
     /**
      * Return the number of elements within the heap.
      *
      * @return Number of elements within the heap.
      */
-    public int size();
+    int size();
 }
