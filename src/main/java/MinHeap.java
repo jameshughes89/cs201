7256e872ceff51ce679220e88b615dcfc1f77c9a
James Hughes <jameshughes89@gmail.com>
2022-11-14 11:00:44 -0400

    constructors

--- src/main/java/MinHeap.java
+++ src/main/java/MinHeap.java
@@ -1,6 +1,21 @@
 import java.util.Iterator;
 
 public class MinHeap<T extends Comparable<? super T>> implements Heap<T> {
+
+    private static final int DEFAULT_CAPACITY = 100;
+
+    private int size;
+    private T[] heap;
+
+    public MinHeap(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public MinHeap(int initialCapacity){
+        heap = (T[]) new Object[initialCapacity];
+        size = 0;
+    }
+
     @Override
     public void add(T element) {
 
