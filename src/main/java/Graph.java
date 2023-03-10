55e43b5e1b7d97b4ad32572ad75f3211b7f23263
James Hughes <jameshughes89@gmail.com>
2023-03-10 09:04:46 -0400

    add exceptions to docs

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
@@ -5,11 +6,11 @@ import java.util.Set;
  * vertex is some arbitrary element often represented as a "node" and an edge is a pair of vertices from the set V
  * indicating some relationship between the vertices. Nodes are considered "neighbours" if they have an edge between
  * them. Consider the below example:
- *
- *  a - b - d
- *   \ /
- *    c
- *
+ * <p>
+ * a - b - d
+ * \ /
+ * c
+ * <p>
  * V = {a, b, c, d}
  * E = {(a, b), (a, c), (b, c), (b, d)}
  *
@@ -29,6 +30,7 @@ public interface Graph<T> {
      *
      * @param node The node to get the neighbours of
      * @return A set of the neighbouring nodes adjacent to the specified node
+     * @throws NoSuchElementException If the specified node does not exist within the graph
      */
     Set<T> getNeighbours(T node);
 
@@ -44,15 +46,17 @@ public interface Graph<T> {
      *
      * @param node The node to get the edges of
      * @return A set of the edges incident on the specified node
+     * @throws NoSuchElementException If the specified node does not exist within the graph
      */
     Set<Edge<T>> getEdges(T node);
 
     /**
      * Return the degree of the specified node. The degree is the number of edges the node has. If the specified node
-     * has an edge to itself, that edge is counted twice.  
+     * has an edge to itself, that edge is counted twice.
      *
      * @param node The node to get the degree of
      * @return The degree of the specified node
+     * @throws NoSuchElementException If the specified node does not exist within the graph
      */
     int getDegree(T node);
 }
