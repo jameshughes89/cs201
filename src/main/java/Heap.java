90a0a77eb27133115dee2525087ba26566c47b76
James Hughes <jameshughes89@gmail.com>
2023-04-13 11:42:15 -0300

    class doc

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,9 +1,21 @@
 import java.util.NoSuchElementException;
 
+/**
+ * A heap with the ordering defined by either the natural ordering of the elements within the heap or by a
+ * Comparator, which can be provided to a constructor. The top of the heap will always be the minimum element, as
+ * defined by the ordering.
+ * <p>
+ * If no Comparator is provided to the constructor, the heap will order the elements based on the natural ordering of
+ * the elements. This means that the elements within the heap must be comparable. If a Comparator is provided, the
+ * elements are ordered based on the Comparator, regardless of the natural ordering of the elements.
+ *
+ * @param <T> Type of elements that are to be in the heap. If a comparator is not provided, then T must be comparable
+ *            in order to use the natural ordering of the elements.
+ */
 public class Heap<T> {
 
     /**
-     * Add the provided element to the heap such that the heap ordering is preserved.
+     * Add the provided element to the heap.
      *
      * @param element Element to be added to the heap
      * @return true if the element was added successfully, false otherwise
@@ -13,8 +25,7 @@ public class Heap<T> {
     }
 
     /**
-     * Remove the element at the top of the heap. This may be the min or max element depending on the specific heap
-     * implementation.
+     * Remove and return the element at the top of the heap.
      *
      * @return Element at the top of the heap
      * @throws NoSuchElementException If the heap is empty
@@ -24,8 +35,7 @@ public class Heap<T> {
     }
 
     /**
-     * Return the element at the top of the heap, but leave it at the top of the heap. This element may be the min or
-     * max element depending on the specific heap implementation.
+     * Return, but do not remove, the element at the top of the heap.
      *
      * @return Element at the top of the heap
      * @throws NoSuchElementException If the heap is empty
