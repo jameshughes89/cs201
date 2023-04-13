643c764702aab2573e3b70a2f0871f4ac8849fd2
James Hughes <jameshughes89@gmail.com>
2023-04-13 11:23:44 -0300

    update comments and change to class

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,10 +1,12 @@
+import java.util.NoSuchElementException;
+
 public interface Heap<T extends Comparable<? super T>> {
 
     /**
      * Add the provided element to the heap such that the heap ordering is preserved.
      *
-     * @param element Element to be added to the heap.
-     * @return true if the element was added successfully, false otherwise.
+     * @param element Element to be added to the heap
+     * @return true if the element was added successfully, false otherwise
      */
     boolean add(T element);
 
@@ -12,7 +14,8 @@ public interface Heap<T extends Comparable<? super T>> {
      * Remove the element at the top of the heap. This may be the min or max element depending on the specific heap
      * implementation.
      *
-     * @return Element at the top of the heap.
+     * @return Element at the top of the heap
+     * @throws NoSuchElementException If the heap is empty
      */
     T remove();
 
@@ -20,21 +23,22 @@ public interface Heap<T extends Comparable<? super T>> {
      * Return the element at the top of the heap, but leave it at the top of the heap. This element may be the min or
      * max element depending on the specific heap implementation.
      *
-     * @return Element at the top of the heap.
+     * @return Element at the top of the heap
+     * @throws NoSuchElementException If the heap is empty
      */
     T peek();
 
     /**
      * Checks if the heap is empty.
      *
-     * @return true if the heap is empty, false otherwise.
+     * @return true if the heap is empty, false otherwise
      */
     boolean isEmpty();
 
     /**
      * Return the number of elements within the heap.
      *
-     * @return Number of elements within the heap.
+     * @return Number of elements within the heap
      */
     int size();
 }
