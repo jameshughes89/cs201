81de2562ecaa7e6fa4518a3ab0130042b4224c11
James Hughes <jameshughes89@gmail.com>
2023-04-13 14:03:53 -0300

    write class

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 
 /**
@@ -6,14 +8,84 @@ import java.util.NoSuchElementException;
  * defined by the ordering.
  * <p>
  * If no Comparator is provided to the constructor, the heap will order the elements based on the natural ordering of
- * the elements. This means that the elements within the heap must be comparable. If a Comparator is provided, the
- * elements do not need to be comparable as the elements are ordered based on the Comparator, regardless of the
- * natural ordering of the elements that may exist.
+ * the elements. This means that the elements within the heap must be comparable, otherwise adding to the heap will
+ * cause a ClassCastException to be thrown. If a Comparator is provided, the elements do not need to be comparable as
+ * the elements are ordered based on the Comparator, regardless of the natural ordering of the elements that may exist.
  *
  * @param <T> Type of elements that are to be in the heap. If a comparator is not provided, then T must be comparable
- *            in order to use the natural ordering of the elements.
+ *            in order to use the natural ordering of the elements, otherwise a ClassCastException is thrown when
+ *            adding.
  */
 public class Heap<T> {
+
+    private static final int DEFAULT_CAPACITY = 100;
+
+    private int size;
+    private T[] heap;
+    private Comparator<T> comparator;
+
+    /**
+     * Create a heap with a natural order comparator defining the ordering of the elements..
+     */
+    public Heap() {
+        this((Comparator<T>) Comparator.naturalOrder());
+    }
+
+    /**
+     * Create a heap with the specified comparator defining the ordering of the elements.
+     *
+     * @param comparator Comparator defining the order of the elements in the heap
+     */
+    public Heap(Comparator<T> comparator) {
+        size = 0;
+        heap = (T[]) new Object[DEFAULT_CAPACITY];
+        this.comparator = comparator;
+    }
+
+    private void bubbleUp(int index) {
+        int currentIndex = index;
+        int parentIndex = getParent(size);
+        while (currentIndex != 0 && comparator.compare(heap[currentIndex], heap[parentIndex]) < 0) {
+            swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = getParent(currentIndex);
+        }
+    }
+
+    private void bubbleDown(int index) {
+        int largerChild = index;
+        int leftChild = getLeft(index);
+        int rightChild = getRight(index);
+        int comparison = comparator.compare(heap[largerChild], heap[leftChild]);
+        if (leftChild < size() && comparison > 0) {
+            largerChild = leftChild;
+        }
+        if (rightChild < size() && comparison > 0) {
+            largerChild = rightChild;
+        }
+        if (largerChild != index) {
+            swap(index, largerChild);
+            bubbleDown(largerChild);
+        }
+    }
+
+    private void swap(int currentIndex, int parentIndex) {
+        T temp = heap[parentIndex];
+        heap[parentIndex] = heap[currentIndex];
+        heap[currentIndex] = temp;
+    }
+
+    private int getParent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int getLeft(int index) {
+        return 2 * index + 1;
+    }
+
+    private int getRight(int index) {
+        return 2 * index + 2;
+    }
 
     /**
      * Add the provided element to the heap.
@@ -22,7 +94,13 @@ public class Heap<T> {
      * @return true if the element was added successfully, false otherwise
      */
     boolean add(T element) {
-        return false;
+        if (size() == heap.length) {
+            heap = Arrays.copyOf(heap, heap.length * 2);
+        }
+        heap[size] = element;
+        bubbleUp(size);
+        size++;
+        return true;
     }
 
     /**
@@ -32,7 +110,15 @@ public class Heap<T> {
      * @throws NoSuchElementException If the heap is empty
      */
     T remove() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty Heap");
+        }
+        T toReturn = heap[0];
+        heap[0] = heap[size - 1];
+        heap[size - 1] = null;
+        size--;
+        bubbleDown(0);
+        return toReturn;
     }
 
     /**
@@ -42,7 +128,10 @@ public class Heap<T> {
      * @throws NoSuchElementException If the heap is empty
      */
     T peek() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty Heap");
+        }
+        return heap[0];
     }
 
     /**
@@ -51,7 +140,7 @@ public class Heap<T> {
      * @return true if the heap is empty, false otherwise
      */
     boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     /**
@@ -60,6 +149,6 @@ public class Heap<T> {
      * @return Number of elements within the heap
      */
     int size() {
-        return 0;
+        return size;
     }
 }
