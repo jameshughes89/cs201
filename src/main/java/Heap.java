10629cdaedb6ed2ea7ce8eced8699f984337c8fa
James Hughes <jameshughes89@gmail.com>
2023-04-27 12:00:13 -0300

    heap class

--- src/main/java/Heap.java
+++ src/main/java/Heap.java
@@ -1,19 +1,76 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 /**
- * A heap with the ordering defined by either the natural ordering of the elements within the heap or by a
- * Comparator, which can be provided to a constructor. The top of the heap will always be the minimum element, as
- * defined by the ordering.
- * <p>
- * If no Comparator is provided to the constructor, the heap will order the elements based on the natural ordering of
- * the elements. This means that the elements within the heap must be comparable. If a Comparator is provided, the
- * elements do not need to be comparable as the elements are ordered based on the Comparator, regardless of the
- * natural ordering of the elements that may exist.
+ * A heap with the ordering defined by a Comparator, which is to be provided to the constructor. The top of the heap
+ * will always be the minimum element, as defined by the ordering.
  *
- * @param <T> Type of elements that are to be in the heap. If a comparator is not provided, then T must be comparable
- *            in order to use the natural ordering of the elements.
+ * @param <T> Type of elements that are to be in the heap.
  */
 public class Heap<T> {
+
+    private static final int DEFAULT_CAPACITY = 100;
+
+    private int size;
+    private T[] heap;
+    private Comparator<T> comparator;
+
+    /**
+     * Create a heap with the specified comparator defining the ordering of the elements.
+     *
+     * @param comparator Comparator defining the order of the elements in the heap
+     */
+    public Heap(Comparator<T> comparator) {
+        size = 0;
+        heap = (T[]) new Object[DEFAULT_CAPACITY];
+        this.comparator = comparator;
+    }
+
+    private void bubbleUp(int index) {
+        int currentIndex = index;
+        int parentIndex = getParent(size);
+        while (currentIndex != 0 && comparator.compare(heap[currentIndex], heap[parentIndex]) < 0) {
+            swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = getParent(currentIndex);
+        }
+    }
+
+    private void bubbleDown(int index) {
+        int swapChild = index;
+        int leftChild = getLeft(index);
+        int rightChild = getRight(index);
+        if (leftChild < size() && comparator.compare(heap[swapChild], heap[leftChild]) > 0) {
+            swapChild = leftChild;
+        }
+        if (rightChild < size() && comparator.compare(heap[swapChild], heap[rightChild]) > 0) {
+            swapChild = rightChild;
+        }
+        if (swapChild != index) {
+            swap(index, swapChild);
+            bubbleDown(swapChild);
+        }
+    }
+
+    private void swap(int currentIndex, int parentIndex) {
+        T temp = heap[parentIndex];
+        heap[parentIndex] = heap[currentIndex];
+        heap[currentIndex] = temp;
+    }
+
+    private int getParent(int index) {
+        return (index - 1) / 2;
+    }
+
+    private int getLeft(int index) {
+        return 2 * index + 1;
+    }
+
+    private int getRight(int index) {
+        return 2 * index + 2;
+    }
 
     /**
      * Add the provided element to the heap.
@@ -22,7 +79,13 @@ public class Heap<T> {
      * @return true if the element was added successfully, false otherwise
      */
     boolean add(T element) {
-        return false;
+        if (size() == heap.length) {
+            heap = Arrays.copyOf(heap, heap.length * 2);
+        }
+        heap[size] = element;
+        bubbleUp(size);
+        size++;
+        return true;
     }
 
     /**
@@ -32,7 +95,15 @@ public class Heap<T> {
      * @throws NoSuchElementException If the heap is empty
      */
     T remove() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty Heap");
+        }
+        T toReturn = heap[0];
+        heap[0] = heap[size - 1];
+        heap[size - 1] = null;
+        size--;
+        bubbleDown(0);
+        return toReturn;
     }
 
     /**
@@ -42,7 +113,10 @@ public class Heap<T> {
      * @throws NoSuchElementException If the heap is empty
      */
     T peek() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty Heap");
+        }
+        return heap[0];
     }
 
     /**
@@ -51,7 +125,7 @@ public class Heap<T> {
      * @return true if the heap is empty, false otherwise
      */
     boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     /**
@@ -60,6 +134,41 @@ public class Heap<T> {
      * @return Number of elements within the heap
      */
     int size() {
-        return 0;
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+        for (int i = 0; i < size(); i++) {
+            stringBuilder.append(heap[i]);
+            if (i < size() - 1) {
+                stringBuilder.append(", ");
+            }
+        }
+        stringBuilder.append("]");
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Heap<?> that = (Heap<?>) o;
+        return Arrays.equals(this.heap, 0, this.size(), that.heap, 0, that.size());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(size);
+        for (int i = 0; i < size(); i++) {
+            result = result * 97 + Objects.hashCode(heap[i]);
+        }
+        return result;
     }
 }
---
+++ src/test/java/HeapTest.java
@@ -0,0 +1,283 @@
+import com.google.common.testing.EqualsTester;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+class HeapTest {
+
+    private Heap<Integer> classUnderTest;
+    private Heap<Integer> preState;
+
+    @Test
+    @SuppressWarnings("UnstableApiUsage")
+    void equals_verify_contract() {
+        Heap<Integer> emptyA = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        Heap<Integer> emptyB = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+
+        Heap<Integer> singletonA = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        Heap<Integer> singletonB = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        singletonA.add(10);
+        singletonB.add(10);
+
+        Heap<Integer> manyA = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        Heap<Integer> manyB = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        manyA.add(10);
+        manyA.add(20);
+        manyA.add(10);
+        manyA.add(5);
+        manyA.add(15);
+        manyB.add(10);
+        manyB.add(20);
+        manyB.add(10);
+        manyB.add(5);
+        manyB.add(15);
+
+        Heap<Integer> unequalDifferentValues = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        unequalDifferentValues.add(100);
+        unequalDifferentValues.add(200);
+        unequalDifferentValues.add(100);
+        unequalDifferentValues.add(50);
+        unequalDifferentValues.add(150);
+
+        Heap<Integer> unequalDifferentOrder = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        unequalDifferentOrder.add(5);
+        unequalDifferentOrder.add(10);
+        unequalDifferentOrder.add(10);
+        unequalDifferentOrder.add(15);
+        unequalDifferentOrder.add(20);
+
+        Heap<Integer> unequalDifferentSizes = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        unequalDifferentSizes.add(10);
+        unequalDifferentSizes.add(20);
+        unequalDifferentSizes.add(10);
+        unequalDifferentSizes.add(5);
+
+        Heap<Integer> unequalSomeEqual = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        unequalSomeEqual.add(10);
+        unequalSomeEqual.add(20);
+        unequalSomeEqual.add(10);
+        unequalSomeEqual.add(5);
+        unequalSomeEqual.add(20);
+
+        Heap<Integer> unequalDifferentComparator = new Heap<>((Comparator<Integer>) Comparator.naturalOrder()
+                .reversed());
+        unequalDifferentComparator.add(10);
+        unequalDifferentComparator.add(20);
+        unequalDifferentComparator.add(10);
+        unequalDifferentComparator.add(5);
+        unequalDifferentComparator.add(15);
+
+        new EqualsTester().addEqualityGroup(Heap.class)
+                .addEqualityGroup(emptyA, emptyB)
+                .addEqualityGroup(singletonA, singletonB)
+                .addEqualityGroup(manyA, manyB)
+                .addEqualityGroup(unequalDifferentValues)
+                .addEqualityGroup(unequalDifferentOrder)
+                .addEqualityGroup(unequalDifferentSizes)
+                .addEqualityGroup(unequalSomeEqual)
+                .addEqualityGroup(unequalDifferentComparator)
+                .testEquals();
+    }
+
+    @BeforeEach
+    void createClassUnderTest() {
+        classUnderTest = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+        preState = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+    }
+
+    @Nested
+    class WhenEmpty {
+
+        @Test
+        void add_empty_returnsTrue() {
+            assertThat(classUnderTest.add(99)).isTrue();
+        }
+
+        @Test
+        void remove_empty_throwsNoSuchElementException() {
+            assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(classUnderTest::remove);
+        }
+
+        @Test
+        void peek_empty_throwsNoSuchElementException() {
+            assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(classUnderTest::peek);
+        }
+
+        @Test
+        void size_empty_returnsZero() {
+            assertThat(classUnderTest.size()).isEqualTo(0);
+        }
+
+        @Test
+        void size_empty_unchanged() {
+            classUnderTest.size();
+            assertThat(classUnderTest).isEqualTo(preState);
+        }
+
+        @Test
+        void isEmpty_empty_returnsTrue() {
+            assertThat(classUnderTest.isEmpty()).isTrue();
+        }
+
+        @Test
+        void isEmpty_empty_unchanged() {
+            classUnderTest.isEmpty();
+            assertThat(classUnderTest).isEqualTo(preState);
+        }
+
+        @Test
+        void toString_empty_returnsString() {
+            assertThat(classUnderTest.toString()).isEqualTo("[]");
+        }
+
+        @Nested
+        class WhenSingleton {
+
+            @BeforeEach
+            void addSingleton() {
+                classUnderTest.add(10);
+                preState.add(10);
+            }
+
+            @Test
+            void add_singleton_returnsTrue() {
+                assertThat(classUnderTest.add(99)).isTrue();
+            }
+
+            @Test
+            void remove_singleton_returnsElement() {
+                assertThat(classUnderTest.remove()).isEqualTo(10);
+            }
+
+            @Test
+            void remove_singleton_emptyHeap() {
+                classUnderTest.remove();
+                assertThat(classUnderTest).isEqualTo(new Heap<>((Comparator<Integer>) Comparator.naturalOrder()));
+            }
+
+            @Test
+            void peek_singleton_returnsElement() {
+                assertThat(classUnderTest.peek()).isEqualTo(10);
+            }
+
+            @Test
+            void peek_singleton_unchanged() {
+                classUnderTest.peek();
+                assertThat(classUnderTest).isEqualTo(preState);
+            }
+
+            @Test
+            void size_singleton_returnsSize() {
+                assertThat(classUnderTest.size()).isEqualTo(1);
+            }
+
+            @Test
+            void size_singleton_unchanged() {
+                classUnderTest.size();
+                assertThat(classUnderTest).isEqualTo(preState);
+            }
+
+            @Test
+            void isEmpty_singleton_returnsFalse() {
+                assertThat(classUnderTest.isEmpty()).isFalse();
+            }
+
+            @Test
+            void isEmpty_singleton_unchanged() {
+                classUnderTest.isEmpty();
+                assertThat(classUnderTest).isEqualTo(preState);
+            }
+
+            @Test
+            void toString_singleton_returnsString() {
+                assertThat(classUnderTest.toString()).isEqualTo("[10]");
+            }
+
+            @Nested
+            @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+            class WhenMany {
+
+                @BeforeEach
+                void addMany() {
+                    classUnderTest.add(20);
+                    classUnderTest.add(20);
+                    classUnderTest.add(10);
+                    classUnderTest.add(5);
+                    classUnderTest.add(15);
+                    preState.add(20);
+                    preState.add(20);
+                    preState.add(10);
+                    preState.add(5);
+                    preState.add(15);
+                }
+
+                @Test
+                void add_many_returnsTrue() {
+                    assertThat(classUnderTest.add(99)).isTrue();
+                }
+
+                @Test
+                void remove_many_returnsElement() {
+                    assertThat(classUnderTest.remove()).isEqualTo(5);
+                }
+
+                @Test
+                void remove_many_change() {
+                    Heap<Integer> expected = new Heap<>((Comparator<Integer>) Comparator.naturalOrder());
+                    expected.add(10);
+                    expected.add(10);
+                    expected.add(15);
+                    expected.add(20);
+                    expected.add(20);
+                    classUnderTest.remove();
+                    assertThat(classUnderTest).isEqualTo(expected);
+                }
+
+                @Test
+                void peek_many_returnsElement() {
+                    assertThat(classUnderTest.peek()).isEqualTo(5);
+                }
+
+                @Test
+                void peek_many_unchanged() {
+                    classUnderTest.peek();
+                    assertThat(classUnderTest).isEqualTo(preState);
+                }
+
+                @Test
+                void size_many_returnsSize() {
+                    assertThat(classUnderTest.size()).isEqualTo(6);
+                }
+
+                @Test
+                void size_many_unchanged() {
+                    classUnderTest.size();
+                    assertThat(classUnderTest).isEqualTo(preState);
+                }
+
+                @Test
+                void isEmpty_many_returnsFalse() {
+                    assertThat(classUnderTest.isEmpty()).isFalse();
+                }
+
+                @Test
+                void isEmpty_many_unchanged() {
+                    classUnderTest.isEmpty();
+                    assertThat(classUnderTest).isEqualTo(preState);
+                }
+
+                @Test
+                void toString_many_returnsString() {
+                    assertThat(classUnderTest.toString()).isEqualTo("[5, 10, 15, 20, 10, 20]");
+                }
+            }
+        }
+    }
+}
